432647add1754adbfabe56835944e89ae22d690d
hungry4h <34955197+hungry4h@users.noreply.github.com>
2019-12-09 12:31:33 +0900

    Update svgRenderer.vue

--- src/components/svgRenderer.vue
+++ src/components/svgRenderer.vue
@@ -17,8 +17,6 @@
     :refreshRate="refreshRate"
   )
     svg(
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink= "http://www.w3.org/1999/xlink"
       ref="svg"
       :width="size.w"
       :height="size.h"
